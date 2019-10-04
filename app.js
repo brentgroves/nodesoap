471562ef93e9db0e85f042c0eddf99fa79fd9cab
Brent Groves <brent.groves@gmail.com>
2019-10-04 15:27:29 -0400

    Added record production

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var msg = 'Hello World';
-console.log(msg);
+var soap = require('soap');
+var url = 'http://example.com/wsdl?wsdl';
+
+var args = {name: 'value'};
+soap.createClient(url, function(err, client) {
+  client.MyFunction(args, function(err, result) {
+    console.log(result);
+  });
+});
