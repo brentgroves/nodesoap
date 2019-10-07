23c8b11c3c4185fcf8527d3f39a40e568893ca2e
Brent Groves <brent.groves@gmail.com>
2019-10-07 15:05:06 -0400

    Added record production

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 var soap = require('soap');
-var url = 'http://example.com/wsdl?wsdl';
-
+var url = 'https://api.plexonline.com/DataSource/Service.asmx?WSDL'
+// https://developers.redhat.com/blog/2016/02/19/apps-101-an-absolute-beginners-guide-to-integrating-with-a-node-js-backend/
+//API Blueprint
+var request_data = {
+    'ExecuteDataSourceRequest':{
+        'DataSourceKey':"2272",
+        'InputParameters': {
+            'InputParameter': {
+                'Name':'Workcenter_Key',
+                'Value':'61324',
+                'Required':'false',
+                'Output':'false'
+            }
+        }
+    }
+}
 var args = {name: 'value'};
-soap.createClient(url, function(err, client) {
-  client.MyFunction(args, function(err, result) {
-    console.log(result);
+var plexWSDL = __dirname + '/plex.wsdl'
+
+soap.createClient(plexWSDL, function(err, client) {
+  debugger;
+      // we now have a soapClient - we also need to make sure there's no `err` here. 
+      if (err){
+          return res.status(500).json(err);
+        }
+  debugger;
+  client.setSecurity(new soap.BasicAuthSecurity('BuscheAvillaKorsws@plex.com', '5b11b45-f59f-'));
+  debugger;
+  client.ExecuteDataSource(request_data, function(err, result) {
+      // we now have a soapClient - we also need to make sure there's no `err` here. 
+      if (err){
+          return res.status(500).json(err);
+      }
+      
+    debugger;  
+    console.log(result.ExecuteDataSourceResult.ResultSets.ResultSet[0].Rows.Row[0].Columns.Column[0].Name);
+    var res=result.ExecuteDataSourceResult.ResultSets.ResultSet[0].Rows.Row[0].Columns.Column
+    console.log(res[0].Name)
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].Name);
+    } 
   });
 });
+
